e278c0eef9ee1a7b5640235b708e6e579517f4c0
serewann <serewann@gmail.com>
2016-10-15 16:34:23 +0300

    Make some refactoring

--- src/Request.php
+++ src/Request.php
@@ -10,64 +10,24 @@ use Lisennk\Request\Requests\HttpRequest;
  * Class Request
  * @package Lisennk\Request
  */
-class Request implements RequestInterface
+class Request
 {
     /**
      * @var RequestInterface
      */
-    public $provider;
+    static private $provider;
 
     /**
-     * Request constructor.
+     * @return RequestInterface
      */
-    public function __construct()
+    static public function instance()
     {
-        $cli = new CliRequest();
-        $http = new HttpRequest();
-        $this->provider = $http->notEmpty() ? $http : $cli;
-    }
+        if (!self::$provider) {
+            $cli = new CliRequest();
+            $http = new HttpRequest();
+            self::$provider = $http->notEmpty() ? $http : $cli;
+        }
 
-    /**
-     * Returns request data by its $key
-     *
-     * @param $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        return $this->provider->input($key);
-    }
-
-    /**
-     * Returns data by its key or $default if not found
-     *
-     * @param $key
-     * @param $default
-     * @return mixed
-     */
-    public function input($key, $default)
-    {
-        return $this->provider->input($key, $default);
-    }
-
-    /**
-     * True if $key exists, false if not
-     *
-     * @param $key
-     * @return bool
-     */
-    public function has($key)
-    {
-        return $this->provider->has($key);
-    }
-
-    /**
-     * True if there is any data
-     *
-     * @return bool
-     */
-    public function notEmpty()
-    {
-        return $this->provider->notEmpty();
+        return self::$provider;
     }
 }
